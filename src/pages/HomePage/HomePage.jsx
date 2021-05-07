8321e58895d553c5886e1576caeb05003e90ab8e
nativedev2021 <nativedev2021@gmail.com>
2021-05-08 00:58:19 +0400

    added homepage styles

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Directory from "../../components/Directory/Directory";
+import "./home-page.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default HomePage;
